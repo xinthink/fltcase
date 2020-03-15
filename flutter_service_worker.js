03b3ab05ce70b081eab6f675ab0b4d60b59710c3
xinthink <xinthink@users.noreply.github.com>
2020-03-15 14:34:44 +0000

    Automated deployment: Sun Mar 15 14:34:44 UTC 2020 xinthink/fltprjs@8aba38fc375bdecae2d2eb5441f080ae28aec55f

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -1,7 +1,7 @@
 'use strict';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "/main.dart.js": "b6f07cc3adc5b54967eca3f456e3110e",
+  "/main.dart.js": "ef5549748dca562612a132e1ae62a59d",
 "/index.html": "fc05f9c99868551723d35d80b9ed65f2",
 "/assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
 "/assets/FontManifest.json": "96a0b4ae580dc68ca922121b8ce35155",
